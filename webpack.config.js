5a403d8961cd0515393793e6a3e804468d8eb717
Egemen Can Uze <egemencanuze@gmail.com>
2017-05-23 00:27:28 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
